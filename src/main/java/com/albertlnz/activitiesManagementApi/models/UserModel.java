f9305b8396da5444688a41e10ad6b56ec810721a
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-09 00:35:03 +0200

    ActivityModel w/ UserModel relation OneToMany

--- src/main/java/com/albertlnz/activitiesManagementApi/models/UserModel.java
+++ src/main/java/com/albertlnz/activitiesManagementApi/models/UserModel.java
@@ -1,11 +1,15 @@
 package com.albertlnz.activitiesManagementApi.models;
 
 import java.util.UUID;
+import java.util.List;
+import java.util.ArrayList;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -22,5 +26,8 @@ public class UserModel {
   private String surname;
   private int age;
   private int email;
+
+  @OneToMany(cascade = CascadeType.ALL)
+  private List<ActivityModel> activities = new ArrayList<>();
 
 }
