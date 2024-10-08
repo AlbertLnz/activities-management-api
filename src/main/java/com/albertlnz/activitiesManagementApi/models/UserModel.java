18f5ae1bbbd6cfb8b4107e276e976e6b40b4d130
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-09 01:43:14 +0200

    Fix tables names correctly

--- src/main/java/com/albertlnz/activitiesManagementApi/models/UserModel.java
+++ src/main/java/com/albertlnz/activitiesManagementApi/models/UserModel.java
@@ -13,10 +13,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
+@Table(name = "users")
 @Getter
 @Setter
 public class UserModel {
