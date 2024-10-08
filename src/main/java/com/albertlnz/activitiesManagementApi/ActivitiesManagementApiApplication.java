e7e92a9b5c3517a85aee70c7b8b78b528e80c607
Albert Lanza <albert2000_lanza@hotmail.com>
2024-10-08 23:59:46 +0200

    Starter SpringBoot project w/ web,jpa,mariadb,lombok & actuator

---
+++ src/main/java/com/albertlnz/activitiesManagementApi/ActivitiesManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.albertlnz.activitiesManagementApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActivitiesManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActivitiesManagementApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albertlnz/activitiesManagementApi/ActivitiesManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertlnz.activitiesManagementApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivitiesManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
